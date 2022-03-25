9ae3db29b5f58d7325612b7038a5c64e51ded5aa
ceyewan <cetewan@gmail.com>
2022-03-25 21:16:10 +0800

    22-03-25

--- xv6-labs/Lab1-util/find.c
+++ xv6-labs/Lab1-util/find.c
@@ -3,35 +3,31 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-void my_find(char const *path, char const *name)
+void my_find(char const* path, char const* name)
 {
     int fd;
     char buf[512], *p;
     struct dirent de;
     struct stat st;
     // 无法打开
-    if ((fd = open(path, 0)) == 0)
-    {
+    if ((fd = open(path, 0)) == 0) {
         fprintf(2, "error\n");
         exit(1);
     }
     // 将文件信息写入 st
-    if (fstat(fd, &st) < 0)
-    {
+    if (fstat(fd, &st) < 0) {
         fprintf(2, "error\n");
         close(fd);
         exit(1);
     }
-    switch (st.type)
-    {
+    switch (st.type) {
     // 传入的参数不是文件夹，肯定是不行的
     case T_FILE:
         fprintf(2, "error\n");
         exit(1);
     case T_DIR:
         // 当前的路径太深了
-        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf))
-        {
+        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)) {
             printf("error\n");
             exit(1);
         }
@@ -40,25 +36,21 @@ void my_find(char const *path, char const *name)
         p = buf + strlen(buf);
         *p++ = '/';
         // 遍历当前路径的每个文件（夹）
-        while (read(fd, &de, sizeof(de)) == sizeof(de))
-        {
+        while (read(fd, &de, sizeof(de)) == sizeof(de)) {
             // 不能是 "." 和 ".."
             if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
                 continue;
             // 复制文件（夹）名，如果是将字符串 20-30
             memmove(p, de.name, DIRSIZ);
             p[DIRSIZ] = 0;
-            if (stat(buf, &st) < 0)
-            {
+            if (stat(buf, &st) < 0) {
                 printf("error\n");
                 exit(1);
             }
-            switch (st.type)
-            {
+            switch (st.type) {
             // 新形成的路径是文件
             case T_FILE:
-                if (strcmp(de.name, name) == 0)
-                {
+                if (strcmp(de.name, name) == 0) {
                     printf("%s\n", buf);
                 }
                 break;
@@ -69,11 +61,10 @@ void my_find(char const *path, char const *name)
         }
     }
 }
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
     // 期望参数是 3 个
-    if (argc != 3)
-    {
+    if (argc != 3) {
         fprintf(2, "Usage: find dir file\n");
         exit(1);
     }
--- xv6-labs/Lab1-util/pingpong.c
+++ xv6-labs/Lab1-util/pingpong.c
@@ -2,11 +2,10 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
     // 期望参数的数量是 1
-    if (argc != 1)
-    {
+    if (argc != 1) {
         fprintf(2, "error\n");
         exit(1);
     }
@@ -14,12 +13,10 @@ int main(int argc, char const *argv[])
     int p[2];
     pipe(p);
     int rc = fork();
-    if (rc == 0)
-    {
+    if (rc == 0) {
         // 子进程先从管道中读取一个字节
         char buf[2];
-        if (read(p[0], buf, 1) != 1)
-        {
+        if (read(p[0], buf, 1) != 1) {
             fprintf(2, "error\n");
             exit(1);
         }
@@ -27,21 +24,17 @@ int main(int argc, char const *argv[])
         close(p[0]);
         printf("%d: received ping\n", getpid());
         // 子进程先管道中写入一个字节
-        if (write(p[1], buf, 1) != 1)
-        {
+        if (write(p[1], buf, 1) != 1) {
             fprintf(2, "error\n");
             exit(1);
         }
         // 关闭管道
         close(p[1]);
         exit(0);
-    }
-    else if (rc > 0)
-    {
+    } else if (rc > 0) {
         char buf[2] = "a";
         // 向管道中写入一个字节
-        if (write(p[1], buf, 1) != 1)
-        {
+        if (write(p[1], buf, 1) != 1) {
             fprintf(2, "error\n");
             exit(1);
         }
@@ -49,8 +42,7 @@ int main(int argc, char const *argv[])
         // 等到子进程结束，这样后面读到的一定是子进程写入的而不是刚刚自己写入的
         wait(0);
         // 从管道中读取一个字节
-        if (read(p[0], buf, 1) != 1)
-        {
+        if (read(p[0], buf, 1) != 1) {
             fprintf(2, "error\n");
             exit(1);
         }
@@ -58,9 +50,7 @@ int main(int argc, char const *argv[])
         close(p[0]);
         printf("%d: received pong\n", getpid());
         exit(0);
-    }
-    else
-    {
+    } else {
         fprintf(2, "error\n");
         exit(1);
     }
